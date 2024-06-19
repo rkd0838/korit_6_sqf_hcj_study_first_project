000055cbf0d44413a4e5e29290642deea60e29f0
rkd0838 <rkdrnjs00@gmail.com>
2024-06-19 17:26:30 +0900

    6월 19일 수업분

---
+++ src/ch04/Button.js
@@ -0,0 +1,17 @@
+function Button ({ text, number, setNumber }) {
+
+    const handleClick = () => {
+        if(text === "증가") {
+            setNumber(number =>number + 1);
+        }
+        if(text === "감소") {
+            setNumber(number =>number - 1);
+        }
+
+    }
+
+    return <button onClick={ handleClick }>{ text }</button>
+
+}
+
+export default Button;
